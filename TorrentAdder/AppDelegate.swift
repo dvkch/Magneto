161daf77158d3bfa6d919e76f392f843b98e73b1
Stanislas Chevallier <contact@stanislaschevallier.fr>
2020-09-24 12:07:01 +0200

    Fix Catalyst window scaling

--- TorrentAdder/AppDelegate.swift
+++ TorrentAdder/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         HostnameResolver.shared.start()
 
+        NSObject.fixCatalystScaling()
         SVProgressHUD.setBackgroundColor(.accent)
         SVProgressHUD.setForegroundColor(.textOverAccent)
 
